f0995b7c1d969a927990d3fa693a5439c14736f9
Marvin2099 <53447150+Marvin2099@users.noreply.github.com>
2020-06-11 10:08:36 -0600

    Add files via upload

--- Practica/src/main/java/com/marvin/practica/Controlador.java
+++ Practica/src/main/java/com/marvin/practica/Controlador.java
@@ -1,17 +1,39 @@
 package com.marvin.practica;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.marvin.practica.model.Persona;
 
 @Controller
 public class Controlador {
 
 	@GetMapping("/")
 	public String Inicio(Model modelo) {
-		String sms = "No me compila :(";
-		modelo.addAttribute("Compa", sms);
+		Persona perso = new Persona();
+		perso.setNombre("Marvin");
+		perso.setApellido("Recinos");
+		perso.setTelefono("1231232");
+		perso.setEmail("recinos@gmail.com");
+		
+		Persona perso2 = new Persona();
+		perso2.setNombre("Gustabo");
+		perso2.setApellido("Horacio");
+		perso2.setTelefono("4214334");
+		perso2.setEmail("Gustacio@gmail.com");
+		
+		/*Si da error en el script de abajo hacer este casteo despus del igual(List)*/
+		List personas = Arrays.asList(perso,perso2);
+		
+		
+		String sms = "No me compila hommie:(";
+		//modelo.addAttribute("Compa", sms);	
+		modelo.addAttribute("personas",personas);
+		
 		return "index";
 	}
 	
---
+++ Practica/src/main/java/com/marvin/practica/model/Persona.java
@@ -0,0 +1,34 @@
+package com.marvin.practica.model;
+
+public class Persona {
+
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellido() {
+		return Apellido;
+	}
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	public String getTelefono() {
+		return Telefono;
+	}
+	public void setTelefono(String telefono) {
+		Telefono = telefono;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	private String Nombre;
+	private String Apellido;
+	private String Telefono;
+	private String Email;
+	
+}
